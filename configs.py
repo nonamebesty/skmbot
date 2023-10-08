511d363f01925840c9be5381b9d6cc09c38ba71d
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-10-09 04:38:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,8 +6,8 @@ import os
 class Config(object):
 	API_ID = 1923471
 	API_HASH = "fcdc178451cd234e63faefd38895c991"
-	BOT_TOKEN = "6450997560:AAGpPGIV6m05R9czMfysJ2vsBEwn2ht3HUw"
-	BOT_USERNAME = "ASFileStore1BOt"
+	BOT_TOKEN = "6480396111:AAEKFR48zOmie5Z6bE4QzIYoAaEG9hWGbTk"
+	BOT_USERNAME = "ASFileStore3BOt"
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001814192750"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "880087645"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://leecher:leecher@cluster0.606mkpi.mongodb.net/?retryWrites=true&w=majority")
